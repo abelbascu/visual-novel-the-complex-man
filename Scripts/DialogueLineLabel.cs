7f36b1ffe0e10ffbbb54ce013af6f474e213a9e5
abelbascu <abel.bascu@gmail.com>
2024-10-20 23:28:19 +0200

    BUG FIXED: dialogue text was printed only until half the first line.

--- Scripts/DialogueLineLabel.cs
+++ Scripts/DialogueLineLabel.cs
@@ -8,15 +8,9 @@ public partial class DialogueLineLabel : RichTextLabel, IInteractableUI {
   private const int LINE_SEPARATION = 2;
   public bool IsInteractable => Visible;
 
-
-  // Called when the node enters the scene tree for the first time.
   public override void _Ready() {
 
     CustomMinimumSize = new Vector2(900, 0);
-    SizeFlagsVertical = SizeFlags.ExpandFill;
-    AddThemeConstantOverride("margin_top", 60);
-    AddThemeConstantOverride("margin_bottom", 60);
-    AddThemeConstantOverride("alignment", (int)HorizontalAlignment.Center);
 
     MouseFilter = MouseFilterEnum.Stop;
     SetProcessInput(true);
@@ -27,9 +21,6 @@ public partial class DialogueLineLabel : RichTextLabel, IInteractableUI {
     BbcodeEnabled = true;
     FitContent = true;
     ScrollActive = false;
-
-    // SizeFlagsHorizontal = SizeFlags.ExpandFill;
-    //  SizeFlagsVertical = SizeFlags.ExpandFill;
 
     AddThemeConstantOverride("line_separation", LINE_SEPARATION);
     AddThemeFontSizeOverride("normal_font_size", 55);
@@ -42,21 +33,6 @@ public partial class DialogueLineLabel : RichTextLabel, IInteractableUI {
     Pressed.Invoke();
     return Task.CompletedTask;
   }
-
-
-  //   public override void _GuiInput(InputEvent @event) {
-  //     if (@event is InputEventMouseButton mouseEvent &&
-  //         (mouseEvent.ButtonIndex == MouseButton.Left ||
-  //         mouseEvent.ButtonIndex == MouseButton.Right) &&
-  //         mouseEvent.Pressed) {
-  //       // Check if the click is within the bounds of the Label
-  //       // if (this.GetGlobalRect().HasPoint(GetGlobalMousePosition())) {
-  //       Pressed.Invoke();
-  //       GD.Print("Label area clicked!");
-  //       GetViewport().SetInputAsHandled(); // Prevent the click from propagating
-  //                                          //}
-  //     }
-  //   }
 
   public void OnPressed() {
     DialogueManager.Instance.OnDialogueBoxUIPressed();
